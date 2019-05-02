0d0f4d5f2a5c1794d98d7d222ccfdc6c209949d6
Jen <jenniferanndcb@gmail.com>
2019-05-02 14:04:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls (long_calls)
   long_calls.any? {|long_calls| long_calls.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
